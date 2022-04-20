97a4b2c6eec81d47a969f2c940fc0a9f9418580f
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-20 15:55:27 -0230

    Update Notifs.js

--- Components/Notifs.js
+++ Components/Notifs.js
@@ -3,9 +3,10 @@ import React from "react";
 
 export default function Notifs() {
   return (
-    <div>
+    <div id="notifs">
       <h2>Notifs</h2>
-      <p> This will be our notifs component!</p>
+      <p class='notif'> This will be our notifs component!</p>
+      <p class='notif'> We really do be speaking facts!</p>
     </div>
   );
 }
