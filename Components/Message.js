7dd230757385b865404497398a5800358b258ce3
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-20 15:53:42 -0230

    Update Message.js

--- Components/Message.js
+++ Components/Message.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import './Login.css';
 
 export default function Message() {
   return (
-    <div id="myHead2">
+    <div>
+      <div id='converse'>
+        <h1>Conversations</h1>
+        <p className='convo'>BuddyPalFriendo</p>
+        <p className='convo'>TheManThatGoesAAAAAAAA</p>
+      </div>
+      <div id="messages">
         <h1>Messages</h1>
-        <p id="Text1">Hello</p>
-        <p id="Text2">My parents left me.</p>
-        <p id="Text3">Hows it goin</p>
-    </div>
+        <p className='sentMessage' id='Text1'>Heyo!</p>
+        <p className='recMessage' id='Text2'>My parents left me.</p>
+        <p className='sentMessage' id='Text3'>How ya doin?</p>
+      </div>
+  </div>
   )
 }
