3e37407257fb87fd40e4ddf489f578b8463ed923
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-21 20:14:26 -0230

    Update Message.js

--- Components/Message.js
+++ Components/Message.js
@@ -6,14 +6,15 @@ export default function Message() {
     <div>
       <div id='converse'>
         <h1>Conversations</h1>
-        <p className='convo'>BuddyPalFriendo</p>
+        <p className='convo'>BigKirbyFan</p>
         <p className='convo'>TheManThatGoesAAAAAAAA</p>
       </div>
       <div id="messages">
-        <h1>Messages</h1>
+        <h1>BigKirbyFan</h1>
         <p className='sentMessage' id='Text1'>Heyo!</p>
         <p className='recMessage' id='Text2'>My parents left me.</p>
         <p className='sentMessage' id='Text3'>How ya doin?</p>
+        <input type="text" id='messagebar'></input>
       </div>
   </div>
   )
