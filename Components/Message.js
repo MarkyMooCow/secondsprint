3556ffb0957956de03a7cf7ca3da6a7f5b6465d0
TyEngle <95366088+TyEngle@users.noreply.github.com>
2022-04-22 15:25:14 -0230

    Update Message.js

--- Components/Message.js
+++ Components/Message.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import './Style.css';
+import React from "react";
+import "./Style.css";
+import "../App.css";
+import Drawer from "./Drawer";
 
 export default function Message() {
   return (
     <div>
-      <div id='converse'>
-        <h1>Conversations</h1>
-        <p className='convo'>BigKirbyFan</p>
-        <p className='convo'>TheManThatGoesAAAAAAAA</p>
+      <Drawer />
+      <div id="converse">
+        <h2>Conversations</h2>
+        <p className="convo">
+          <span className="profile1">&#40;pic&#41;</span>BuddyPalFriendo
+        </p>
+        <p className="convo">
+          <span className="profile2">&#40;pic&#41;</span>TheManThatGoesAAAAAAAA
+        </p>
+        <p className="convo">
+          <span className="profile3">&#40;pic&#41;</span>Larry41325
+        </p>
+        <p className="convo">
+          <span className="profile4">&#40;pic&#41;</span>ApplewoodSmokedBacon
+        </p>
+        <p className="convo">
+          <span className="profile5">&#40;pic&#41;</span>DBZF0Lyyyyyfe
+        </p>
       </div>
       <div id="messages">
-        <h1>BigKirbyFan</h1>
-        <p className='sentMessage' id='Text1'>Heyo!</p>
-        <p className='recMessage' id='Text2'>My parents left me.</p>
-        <p className='sentMessage' id='Text3'>How ya doin?</p>
-        <input type="text" id='messagebar'></input>
+        <h2>Messages</h2>
+        <p className="sentMessage">Heyo!</p>
+        <p className="recMessage">
+          <span className="profile1">&#40;pic&#41;</span>My parents left me.
+        </p>
+        <p className="sentMessage">How ya doin?</p>
+        <input
+          type="text"
+          id="messagebar"
+          placeholder="write a message"
+        ></input>
       </div>
-  </div>
-  )
+    </div>
+  );
 }
