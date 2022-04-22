308a668317d2c7463ee112a2935aa74cdd1de4c2
MarkyMooCow <95723859+MarkyMooCow@users.noreply.github.com>
2022-04-22 16:01:58 -0230

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Notifs from "./Components/Notifs";
 import Message from "./Components/Message";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
