7efae0637fb266eba9b4d962297463ecc7a98434
RileyJohn39 <95235543+RileyJohn39@users.noreply.github.com>
2022-04-22 14:14:35 -0230

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,10 +1,43 @@
 import React from "react";
-
+import {
+  FaHome,
+  FaBell,
+  FaEnvelope,
+  FaUser,
+  FaCog,
+  FaBookmark,
+  FaEllipsisH,
+  FaHeart,
+  FaCommentDots,
+  FaShare,
+} from "react-icons/fa";
 export default function Home() {
   return (
-    <div>
-      <h2>Home Component</h2>
-      <p> This is my home compoenent - for application</p>
+    <div id="myHead5">
+      <h2>Home</h2>
+      
+      <p>Username @Username • 10h Ago</p>
+
+      <div class="a"> <p>Yooo this duck is so random!</p> </div>
+      
+      <img src="test.jpg"></img> <br></br>
+      <div style={{ display: "flex" }}>
+        <p class="notif">
+          {" "}
+          <FaHeart style={{ color: "Red" }} /> 100k &nbsp; &nbsp; &nbsp; &nbsp;
+          &nbsp; &nbsp; &nbsp; &nbsp;{" "}
+        </p>
+        <p class="notifs">
+          {" "}
+          <FaShare style={{ color: "Black" }} /> Share{" "}
+        </p>{" "}
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <p class="notifs">
+          {" "}
+          <FaCommentDots style={{ color: "Black" }} /> 10k{" "}
+        </p>{" "}
+        &nbsp;
+      </div>
     </div>
   );
 }
