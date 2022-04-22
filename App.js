1b5902e4d7cd1091b3a4d2b11098d7c2ab1d51b1
TyEngle <95366088+TyEngle@users.noreply.github.com>
2022-04-22 13:59:05 -0230

    Update App.js
    This is my current app.js file
    I don't have the font awesome icons that you guys have so u may have to add those imports you have that are still in use to this file

--- App.js
+++ App.js
@@ -1,65 +1,30 @@
-import Profile from "./Components/Profile";
+import React from "react";
 import Notifs from "./Components/Notifs";
 import Message from "./Components/Message";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import {
-  FaHome,
-  FaBell,
-  FaEnvelope,
-  FaUser,
-  FaCog,
-  FaBookmark,
-  FaEllipsisH,
-} from "react-icons/fa";
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";;
+import { makeStyles } from "@material-ui/core/styles";
 
-// Run 'npm i react-pro-sidebar' in the terminal in order for this ProSidebar stuff to work.
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    width: "1000px",
+  },
+});
 function App() {
+  const classes = useStyles();
   return (
-    <Router>
-      <div>
-        <ProSidebar>
-          <Menu iconShape="square">
-            <MenuItem icon={<FaHome />}>
-              Home
-              <Link to="/Home" />
-            </MenuItem>
-            <MenuItem icon={<FaEnvelope />}>
-              Messages
-              <Link to="/Message" />
-            </MenuItem>
-            <MenuItem icon={<FaBell />}>
-              Notifications
-              <Link to="/Notifs" />
-            </MenuItem>
-            <MenuItem icon={<FaUser />}>
-              Profile
-              <Link to="/Profile" />
-            </MenuItem>
-            <MenuItem icon={<FaCog />}>
-              Settings
-            </MenuItem>
-            <MenuItem icon={<FaBookmark />}>
-              Bookmarks
-            </MenuItem>
-            <MenuItem icon={<FaEllipsisH />}>
-              More
-            </MenuItem>
-          </Menu>
-        </ProSidebar>
-        ;
-      </div>
-      <Routes>
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Notifs" element={<Notifs />} />
-        <Route path="/Message" element={<Message />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </Router>
+    <div className={classes.container}>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/notifs" element={<Notifs />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
